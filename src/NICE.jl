43b4cdf3685a75abd67da8e41b5a633a1cea7827
Michael Richer <richerm@mcmaster.ca>
2022-11-07 21:41:08 -0500

    finished the code (untested)

--- src/NICE.jl
+++ src/NICE.jl
@@ -1,100 +1,180 @@
 module NICE
 
 
-export ReactionSystem
-# export run_kmc, run_nekmc
+export ReactionSystem, run_kmc, run_nekmc
 
 
-# Define the struct with information pertaining to the reaction system
-struct ReactionSystem
+mutable struct ReactionSystem
+
+    # Constants
     n_reaction::Int
     n_species::Int
     stoich::Matrix{Float64}
     rev_rate_consts::Vector{Float64}
     fwd_rate_consts::Vector{Float64}
+
+    # Mutables
+    n_iter::Int
+    time::Float64
+    concs::Vector{Float64}
+    rev_rates::Vector{Float64}
+    fwd_rates::Vector{Float64}
+    net_rates::Vector{Float64}
 end
 
 
-# Constructor #1
-function ReactionSystem(stoich, rev_rate_consts, fwd_rate_consts::AbstractVector{Real})
+function ReactionSystem(stoich, concs, rev_rate_consts, fwd_rate_consts)
     n_reaction = size(stoich, 1)
     n_species = size(stoich, 2)
-    return ReactionSystem(n_reaction, n_species, stoich, rev_rate_consts, fwd_rate_consts)
+    n_iter = 0
+    time = 0.
+    rev_rates = zeros(Float64, n_reaction)
+    fwd_rates = zeros(Float64, n_reaction)
+    net_rates = zeros(Float64, n_reaction)
+    return ReactionSystem(n_reaction, n_species, stoich, rev_rate_consts, fwd_rate_consts,
+        n_iter, time, concs, rev_rates, fwd_rates, net_rates)
 end
 
 
-# Constructor #2
-function ReactionSystem(stoich, keq_vals, φ::Real=1.0)
+function ReactionSystem(stoich, concs, keq_vals, φ::Real=1.0)
     n_reaction = size(stoich, 1)
     n_species = size(stoich, 2)
     rev_rate_consts = φ ./ (keq_vals .+ 1)
     fwd_rate_consts = φ .- rev_rate_consts
-    return ReactionSystem(n_reaction, n_species, stoich, rev_rate_consts, fwd_rate_consts)
+    n_iter = 0
+    time = 0.
+    rev_rates = zeros(Float64, n_reaction)
+    fwd_rates = zeros(Float64, n_reaction)
+    net_rates = zeros(Float64, n_reaction)
+    return ReactionSystem(n_reaction, n_species, stoich, rev_rate_consts, fwd_rate_consts,
+        n_iter, time, concs, rev_rates, fwd_rates, net_rates)
 end
 
 
-# Functions for KMC and NEKMC. There are two definitions for each function name;
-# one for KMC and one for NEKMC. They have different arguments!
-
-
-function update_rates(rxn_system, concs, rev_rates, fwd_rates, net_rates)
-    for i in 1 : rxn_system.n_reaction
-        p = rxn_system.rev_rate_consts[i]
-        q = rxn_system.fwd_rate_consts[i]
-        for j in 1 : rxn_system.n_species
-            s = rxn_system.stoich[i, j]
-            if s >= 0.
-                p *= concs[j] ^ s
-            else
-                q *= concs[j] ^ abs(s)
-            end
-        end
-        rev_rates[i] = p
-        fwd_rates[i] = q
-        net_rates[i] = q - p
+function run_kmc(rxn_system, n_iter, ε=1.0e-4)
+    pvec = zeros(2 * rxn_system.n_reaction)
+    for i in 1 : n_iter
+        update_rates(rxn_system)
+        i_rxn = kmc_select_reaction(rxn_system, pvec)
+        rxn_system.time += kmc_do_reaction(rxn_system, i_rxn, ε)
+        rxn_system.n_iter += 1
     end
 end
 
 
-function select_reaction(rxn_system, rev_rates, fwd_rates)
-    #
-    # ...
-    #
-    return 1 # index of reaction in ``stoich`` matrix.
+function run_nekmc(rxn_system, n_iter, ε=1.0e-4)
+    pvec = zeros(rxn_system.n_reaction)
+    for i in 1 : n_iter
+        update_rates(rxn_system)
+        i_rxn = nemkc_select_reaction(rxn_system, pvec)
+        rxn_system.time += nemkc_do_reaction(rxn_system, i_rxn, ε)
+        rxn_system.n_iter += 1
+    end
 end
 
 
-function select_reaction(rxn_system, net_rates)
-    #
-    # ...
-    #
-    return 1 # index of reaction in ``stoich`` matrix.
+function update_rates(rxn_system)
+    for i in 1 : rxn_system.n_reaction
+        rev_rate = rxn_system.rev_rate_consts[i]
+        fwd_rate = rxn_system.fwd_rate_consts[i]
+        for j in 1 : rxn_system.n_species
+            s = rxn_system.stoich[i, j]
+            if s >= 0.
+                rev_rate *= rxn_system.concs[j] ^ s
+            else
+                fwd_rate *= rxn_system.concs[j] ^ abs(s)
+            end
+        end
+        rxn_system.rev_rates[i] = rev_rate
+        rxn_system.fwd_rates[i] = fwd_rate
+        rxn_system.net_rates[i] = fwd_rate - rev_rate
+    end
 end
 
 
-function do_reaction(rxn_system, concs, rev_rates, fwd_rates, ε)
-    #
-    # Update the ``concs`` vector.
-    # ε is the concentration step size.
-    #
+function kmc_select_reaction(rxn_system, pvec)
+    # Update probability vector
+    p = 0.
+    for i in 1 : rxn_system.n_reaction
+        p += rxn_system.fwd_rates[i]
+        pvec[i] = p
+    end
+    for i in 1 : rxn_system.n_reaction
+        p += rxn_system.rev_rates[i]
+        pvec[i + rxn_system.n_reaction] = p
+    end
+    # Select random reaction
+    p *= rand(Float64)
+    for i in 1 : 2 * rxn_system.n_reaction
+        if pvec[i] > p
+            return i
+        end
+    end
+    # sentinel return value
+    return 2 * rxn_system.n_reaction
 end
 
 
-function do_reaction(rxn_system, concs, net_rates, ε)
-    #
-    # Update the ``concs`` vector.
-    # ε is the concentration step size.
-    #
+function nekmc_select_reaction(rxn_system, pvec)
+    # Update probability vector
+    p = 0.
+    for i in 1 : rxn_system.n_reaction
+        p += abs(rxn_system.net_rates[i])
+        pvec[i] = p
+    end
+    # Select random reaction
+    p *= rand(Float64)
+    for i in 1 : rxn_system.n_reaction
+        if pvec[i] > p
+            return i
+        end
+    end
+    # sentinel return value
+    return rxn_system.n_reaction
 end
 
 
-# And now write the entire (NE)KMC algorithm from these pieces:
-#
-# function run_(ne)kmc( ..., ..., ..., ... )
-#     ...
-#     ...
-#     ...
-# end
+function kmc_do_reaction(rxn_system, i_rxn, ε)
+    # Forward reaction (i_rxn <= n_rxn)
+    if i_rxn <= rxn_system.n_reaction
+        rate = rxn_system.fwd_rates[i_rxn]
+        if rate >= 0
+            for j = 1 : rxn_system.n_species
+                rxn_system.concs[j] += rxn_system.stoich[i_rxn, j] * ε
+            end
+            return ε / rate
+        end
+    # Reverse reaction (i_rxn > n_rxn)
+    else
+        i_rxn -= rxn_system.n_reaction
+        rate = rxn_system.rev_rates[i_rxn]
+        if rate >= 0
+            for j = 1 : rxn_system.n_species
+                rxn_system.concs[j] -= rxn_system.stoich[i_rxn, j] * ε
+            end
+            return ε / rate
+        end
+    end
+    # Sentinel return value
+    return 0.
+end
+
+
+function nekmc_do_reaction(rxn_system, i_rxn, ε)
+    # Net reaction
+    rate = rxn_system.net_rates[i_rxn]
+    if rate >= 0
+        for j = 1 : rxn_system.n_species
+            rxn_system.concs[j] += rxn_system.stoich[i_rxn, j] * ε
+        end
+        return ε / rate
+    else
+        for j = 1 : rxn_system.n_species
+            rxn_system.concs[j] -= rxn_system.stoich[i_rxn, j] * ε
+        end
+        return -ε / rate
+    end
+end
 
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using Test
     # Write your tests here.
     #
     # I just checked that it runs...
-    a = ReactionSystem(zeros(3,3), zeros(3))
+    a = ReactionSystem(zeros(3,3), zeros(3), zeros(3))
+    a = ReactionSystem(zeros(3,3), zeros(3), zeros(3), zeros(3))
     @show a
 end
